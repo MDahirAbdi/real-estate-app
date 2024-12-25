27f14fb684f909db255d9cffea6f71c38f8eb0b6
Mohamud Dahir <siyaasi54@gmail.com>
2024-12-25 22:24:44 +0300

    project setup

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,21 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className=" text-lg font-bold text-red-600">Welcome to the App</Text>
+
+      <Link href="/sign-in">Sign-in</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Property</Link>
+    </View>
+  );
+}
